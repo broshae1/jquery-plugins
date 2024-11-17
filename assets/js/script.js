8ae761fb904a450e4c7f54830a037405f3649882
Kerrin <hello.kerrin@gmail.com>
2024-11-17 18:15:07 -0500

    no mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,30 @@ else {
     console.log("no jquery")
 }
 
-let mobileMenuOpen = false;
+// let mobileMenuOpen = false;
 
-$("#mobile-menu-button").click(function(){
+// $("#mobile-menu-button").click(function(){
     
-    if(mobileMenuOpen == false) {
-        $("#main-menu").removeClass("display-none-mobile");
-        $("#mobile-menu-text").text("Close");
-        let close = $("#mobile-menu-icon").attr("src").replace("menu", "close");
-        console.log(close);
-        $("#mobile-menu-icon").attr("src", close);
+//     if(mobileMenuOpen == false) {
+//         $("#main-menu").removeClass("display-none-mobile");
+//         $("#mobile-menu-text").text("Close");
+//         let close = $("#mobile-menu-icon").attr("src").replace("menu", "close");
+//         console.log(close);
+//         $("#mobile-menu-icon").attr("src", close);
 
-        mobileMenuOpen = true;
-    }
+//         mobileMenuOpen = true;
+//     }
 
-    else {
-        $("#main-menu").addClass("display-none-mobile");
-        $("#mobile-menu-text").text("Menu");
-        let open = $("#mobile-menu-icon").attr("src").replace("close", "menu");
+//     else {
+//         $("#main-menu").addClass("display-none-mobile");
+//         $("#mobile-menu-text").text("Menu");
+//         let open = $("#mobile-menu-icon").attr("src").replace("close", "menu");
 
-        $("#mobile-menu-icon").attr("src", open);
+//         $("#mobile-menu-icon").attr("src", open);
 
-        mobileMenuOpen = false; 
-    }
-});
+//         mobileMenuOpen = false; 
+//     }
+// });
 
 // $("a[data-bakery-image]").click(function(){
 //  let image = $(this).children("img").attr("src");
