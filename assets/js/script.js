9d7c4c23545cbcec023a4a1d0bf3d5faaa49d39e
Kerrin <hello.kerrin@gmail.com>
2024-12-01 16:57:58 -0500

    AOS

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,28 @@ new Splide( '.splide', {
     type   : 'loop',
     heightRatio: 0.5,
 } ).mount();
+
+AOS.init({
+    offset: 200,
+    duration: 1000,
+    delay: 300
+})
+
+
+$.modal.defaults = {
+    closeExisting: true,    // Close existing modals. Set this to false if you need to stack multiple modal instances.
+    escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
+    clickClose: true,       // Allows the user to close the modal by clicking the overlay
+    closeText: 'Close',     // Text content for the close <a> tag.
+    closeClass: '',         // Add additional class(es) to the close <a> tag.
+    showClose: true,        // Shows a (X) icon/link in the top-right corner
+    modalClass: "modal",    // CSS class added to the element being displayed in the modal.
+    blockerClass: "jquery-modal",  // CSS class added to the overlay (blocker).
+  
+    // HTML appended to the default spinner during AJAX requests.
+    spinnerHtml: '<div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div>',
+  
+    showSpinner: true,      // Enable/disable the default spinner during AJAX requests.
+    fadeDuration: 300,     // Number of milliseconds the fade transition takes (null means no transition)
+    fadeDelay: 1.0          // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
+  };
