426c4c49e0c6c7e927114b11fe83dcaef9d6303d
Kerrin <hello.kerrin@gmail.com>
2024-12-01 20:12:36 -0500

    complete2

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// defaults for jquery modal
+
+$.modal.defaults = {
+    closeExisting: true,    // Close existing modals. Set this to false if you need to stack multiple modal instances.
+    escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
+    clickClose: true,       // Allows the user to close the modal by clicking the overlay
+    closeText: 'Close',     // Text content for the close <a> tag.
+    closeClass: '',         // Add additional class(es) to the close <a> tag.
+    showClose: true,        // Shows a (X) icon/link in the top-right corner
+    modalClass: "modal",    // CSS class added to the element being displayed in the modal.
+  
+    // HTML appended to the default spinner during AJAX requests.
+    spinnerHtml: '<div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div>',
+  
+    showSpinner: true,      // Enable/disable the default spinner during AJAX requests.
+    fadeDuration: 500,     // Number of milliseconds the fade transition takes (null means no transition)
+    fadeDelay: 1.0          // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
+  };
+
+//   creating new carousel
+  new Splide( '#image-carousel', {
+    heightRatio: 0.6,
+    type: 'loop',
+    perPage: 2,
+    gap: '50px'
+  } ).mount();
+
+//   Animate on Scroll
+AOS.init({
+    duration: 1000
+});
